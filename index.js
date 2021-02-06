abe55220f9ff40e55268d267d70462ecd249b6fb
johnjohnn <conde2010@aol.com>
2021-02-06 02:42:22 -0500

    adding the key

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = "jonathan";
+
+const question = () => {
+alert("what is your name");
+if (name === jonathan) {
+    alert("the name is correct")
+}else {
+    alert("the name you entered is wrong")
+}
+
+
+}
